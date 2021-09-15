86737324f607024e2ac3d146a752d1747ec31321
wnipuna <nipuna.r3@gmail.com>
2021-09-15 10:03:12 +0530

    student access

--- src/main/java/com/nipuna/spring/controller/StudentController.java
+++ src/main/java/com/nipuna/spring/controller/StudentController.java
@@ -4,17 +4,20 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nipuna.spring.dao.StudentRepo;
 import com.nipuna.spring.models.Student;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class StudentController {
 
